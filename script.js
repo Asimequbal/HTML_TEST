1af4f82173df60b8120d4de405bad24af2097452
Asim Equbal <114874702+Asimequbal@users.noreply.github.com>
2023-02-28 09:31:21 +0530

    JS form Validatiion

---
+++ script.js
@@ -0,0 +1,34 @@
+function validateForm() {
+    console.log('validate form init')
+    let fullName = document.querySelector('#full_name').value
+    let fullNameSize = fullName.length
+    console.log(fullNameSize)
+
+    let phoneNumber = document.querySelector('#phone_number').value
+    let phoneNumberSize = phoneNumber.length
+
+    let message = document.querySelector('#your_message').value
+    let messageSize = message.length
+
+    let emailId = document.querySelector('#email_id').value
+    let emailFormat = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+    if (fullNameSize <= 3) {
+        document.querySelector('.error_message').innerText = 'Name is required with minium 4 characters!!'
+
+    } else if (!emailId.match(emailFormat)) {
+        document.querySelector('.error_message').innerText = 'This is not valid email id'
+
+    } else if (phoneNumberSize !== 10) {
+        document.querySelector('.error_message').innerText = 'Phone number should be 10 digit'
+
+    } else if (messageSize <= 10) {
+        document.querySelector('.error_message').innerText = 'Message is required with minium 10 characters!!'
+
+    } else {
+        console.log('not matching...')
+        document.querySelector('.error_message').innerText = ''
+        document.querySelector('.success_message').innerText = 'Submit Successfully'
+    }
+
+}
